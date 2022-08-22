ee1616a64ce8e8368baa775ee24a46866683ca66
Faisal Nisar <fnisar394@protonmail.com>
2022-08-22 11:49:58 +0500

    modifier completed. it will only subtract the time for now.

--- modifier.go
+++ modifier.go
@@ -5,52 +5,116 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
-	// if len(os.Args) < 3 {
-	// 	fmt.Println("Usage: go run modifier.go <inputfile> <outputfile>")
-	// }
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run modifier.go <inputfile> <outputfile> <modify value>")
+		return
+	}
 
-	// inputFile := os.Args[1]
-	// outputFile := os.Args[2]
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+	modifyValue, _ := strconv.Atoi(os.Args[3])
 
-	// fmt.Printf("input file: %v\n", inputFile)
-	// fmt.Printf("output file: %v\n", outputFile)
+	fmt.Printf("Processing %v\n", inputFile)
 
-	f, err := os.Open("./input/Hz.Omer.S01E01.srt")
+	inpFile, err := os.Open(inputFile)
 
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 
-	defer f.Close()
+	fmt.Printf("Creating %v if not exists\n", outputFile)
+	outFile, err := os.Create(outputFile)
 
-	scanner := bufio.NewScanner(f)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer inpFile.Close()
+	defer outFile.Close()
+
+	scanner := bufio.NewScanner(inpFile)
 
 	subtitleCounter := 0
+	regexPattern, _ := regexp.Compile("[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}")
 
 	for scanner.Scan() {
-		// fmt.Println(scanner.Text())
 
-		if subtitleCounter%4 == 1 {
-			fmt.Printf("time -> %v\n", scanner.Text())
-			// TODO: split the time into start time and end time
+		text := scanner.Text()
 
-			// TODO: subtract time from start time
+		if regexPattern.MatchString(text) {
+			// split the time into start time and end time
+			times := strings.Split(text, " ")
+			startTime := times[0]
+			endTime := times[2]
 
-			// TODO: subtract time from end time
+			// subtract time from start time
+			modifiedStartTime := addValueToTime(startTime, modifyValue)
 
-			// TODO: combines the time
+			// subtract time from end time
+			modifiedEndTime := addValueToTime(endTime, modifyValue)
+
+			// combines the time
+			text = modifiedStartTime + " --> " + modifiedEndTime
 		}
-		// TODO: write processed information into file
-
+		// write processed information into file
+		outFile.WriteString(text + "\n")
 		subtitleCounter += 1
 	}
 
+	fmt.Printf("Processed %v\n", inputFile)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+// currently supports subtraction of time
+func addValueToTime(time string, value int) string {
+	parts := strings.Split(time, ",")
+
+	timeParts := strings.Split(parts[0], ":")
+
+	sec, _ := strconv.Atoi(timeParts[2])
+	min, _ := strconv.Atoi(timeParts[1])
+	hrs, _ := strconv.Atoi(timeParts[0])
+
+	sec += value
+
+	if sec < 0 {
+		sec += 60
+		min -= 1
+		if min < 0 {
+			min += 60
+			hrs -= 1
+		}
+	}
+
+	if hrs < 10 {
+		timeParts[0] = "0" + strconv.Itoa(hrs)
+	} else {
+		timeParts[0] = strconv.Itoa(hrs)
+	}
+
+	if min < 10 {
+		timeParts[1] = "0" + strconv.Itoa(min)
+	} else {
+		timeParts[1] = strconv.Itoa(min)
+	}
+
+	if sec < 10 {
+		timeParts[2] = "0" + strconv.Itoa(sec)
+	} else {
+		timeParts[2] = strconv.Itoa(sec)
+	}
+
+	return timeParts[0] + ":" + timeParts[1] + ":" + timeParts[2] + "," + parts[1]
 }
